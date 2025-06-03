9008e314d92e7563c8926db4be493cbb66a8b0b3
Alberto Estepa <alberto16166@gmail.com>
2025-06-03 19:56:13 +0200

    commiteo mis cambios

--- js/script.js
+++ js/script.js
@@ -1,42 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-  function dropHandler(ev) {
-    console.log("File(s) dropped");
-    ev.preventDefault();
-
-    // Quitar la clase visual
-    ev.target.classList.remove("drag-over");
-
-    if (ev.dataTransfer.items) {
-      [...ev.dataTransfer.items].forEach((item, i) => {
-        if (item.kind === "file") {
-          const file = item.getAsFile();
-          console.log(`… file[${i}].name = ${file.name}`);
-        }
-      });
-    } else {
-      [...ev.dataTransfer.files].forEach((file, i) => {
-        console.log(`… file[${i}].name = ${file.name}`);
-      });
-    }
-  }
-
-  function dragOverHandler(ev) {
-    ev.preventDefault();
-    console.log("File(s) in drop zone");
-
-    ev.target.classList.add("drag-over"); // Añade la clase mientras el archivo está encima
-  }
-
-  function dragEnterHandler(ev) {
-    ev.preventDefault();
-    ev.target.classList.add("drag-over");
-  }
-
-  function dragLeaveHandler(ev) {
-    ev.target.classList.remove("drag-over"); // Quita la clase cuando el archivo sale
-  }
-  
 
 
   // === GIF CARRUSEL EN CABECERA ===
