7ed0f0f921c0b65df5285a03876be65285229a38
Alberto Estepa <alberto16166@gmail.com>
2025-06-04 19:57:45 +0200

    cambios tiempo real

--- js/viewerStyle.js
+++ js/viewerStyle.js
@@ -1,5 +1,18 @@
 let btnOptions = document.getElementById('options');
-
+let form;
+import {actualizarModelo} from '/js/scene.js';
 btnOptions.addEventListener('click',function(){
-    
+
+});
+
+form = document.getElementById('formStyles');
+form.addEventListener('input',function(){
+   const datos = Object.fromEntries(new FormData(form).entries());
+
+    datos.roughness=parseFloat(datos.roughness)/1000;
+    datos.metalness=parseFloat(datos.metalness)/1000;
+    sessionStorage.setItem('estilos',JSON.stringify(datos))
+    actualizarModelo();
+    console.log(form);
+    console.log(`desde viewerStyle.js: datos.roughnes= ${datos.roughness}`)
 });
